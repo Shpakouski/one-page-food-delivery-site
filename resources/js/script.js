847cd915fe752875e48125cb81783fc8d05a0f55
Shpakouski <job.shpakouski@gmail.com>
2020-12-21 10:58:29 +0300

    favicons

--- resources/js/script.js
+++ resources/js/script.js
@@ -87,4 +87,19 @@ $(document).ready(function () {
         offset: '60%'
     });
 
+    /* Mobile navigation*/
+    $('.js--nav-icon').click(function () {
+        var nav = $('.js--main-nav');
+        var icon = $('.js--nav-icon i');
+
+        nav.slideToggle(200);
+        if (icon.hasClass('fa-bars')) {
+            icon.addClass('fa-minus');
+            icon.removeClass('fa-bars');
+        } else {
+            icon.addClass('fa-bars');
+            icon.removeClass('fa-minus');
+        }
+    });
+
 });
